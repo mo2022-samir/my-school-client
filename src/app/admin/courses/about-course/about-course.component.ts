5632b0d7ae14ec20d6cc3b3641139916068b6c39
mo2022-samir <mohammedsamir20220@gmail.com>
2023-01-11 16:36:04 +0200

    Handling courses section

--- src/app/admin/courses/about-course/about-course.component.ts
+++ src/app/admin/courses/about-course/about-course.component.ts
@@ -1,4 +1,6 @@
+import { CoursesService } from 'src/app/core/service/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-about-course',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-course.component.sass'],
 })
 export class AboutCourseComponent implements OnInit {
+  num: number = 0;
+  selectedCourse: any;
+  courseData: any;
+  coursesList: Observable<any>;
   breadscrums = [
     {
       title: 'About Course',
@@ -13,7 +19,20 @@ export class AboutCourseComponent implements OnInit {
       active: 'About Course',
     },
   ];
-  constructor() {}
+  constructor(private coursesService: CoursesService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getList();
+  }
+  getList(): void {
+    this.coursesList = this.coursesService.getCoursesList();
+  }
+  onChange(): void {
+    this.getCourseDetails(this.selectedCourse);
+  }
+  getCourseDetails(id: string) {
+    this.coursesService.getCourseById(id).subscribe((res) => {
+      this.courseData = res;
+    });
+  }
 }
