402ef967820009d92130bf68a8f5f93ac4193309
Ahmmed-Hassan <ahmednhoo@gmail.com>
2023-01-17 20:54:55 +0200

    teacher setting and student settings done

--- src/app/teacher/settings/settings.component.ts
+++ src/app/teacher/settings/settings.component.ts
@@ -1,4 +1,6 @@
+import { TeacherService } from './../../core/service/teacher.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.sass'],
 })
 export class SettingsComponent implements OnInit {
+  securityForm :FormGroup;
+  settingsForm: FormGroup;
+  userInfo: any;
+  userData =JSON.parse(localStorage.getItem("currentUser")) 
   breadscrums = [
     {
       title: 'Settings',
@@ -14,7 +20,35 @@ export class SettingsComponent implements OnInit {
     },
   ];
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(private teacherService:TeacherService,private fb: FormBuilder) {
+  }
+  form1() {
+    this.securityForm = this.fb.group({
+      username: [this.userInfo?.user.username, [Validators.required]],
+      password: [this.userInfo?.user.password, Validators.required],
+    });
+  }
+  form2() {
+    this.settingsForm = this.fb.group({
+      firstName: [this.userInfo?.user.firstName, [Validators.required]],
+      lastName: [this.userInfo?.user.lastName, Validators.required],
+      email: [this.userInfo?.user.email, [Validators.email,Validators.required]],
+      address: [this.userInfo?.user.address, Validators.required],
+    });
+  }
+  onSubmitForm1(){
+    this.teacherService.editTeacher(this.userData.id, this.securityForm.value).subscribe()
+  }
+  onSubmitForm2(){
+    this.teacherService.editTeacher(this.userData.id, this.settingsForm.value).subscribe()
+  }
+  ngOnInit(): void {
+  this.form1();
+  this.form2();
+    this.teacherService.getTeacherById(this.userData.id).subscribe(
+      res=> {this.userInfo = res;
+      this.form1();
+    this.form2()}
+    )
+  }
 }
